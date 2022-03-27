99c0a09196379cca7cc2ca91729e58b382c3d116
Arif Fatchurohman <arif.f.ft20@mail.umy.ac.id>
2022-03-27 23:07:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todoaplikasi/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.todoaplikasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.content.Intent;
+import
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         txUsername = findViewById(R.id.);
+    }
+}
